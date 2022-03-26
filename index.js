6e64c24b6463b04973dba42f74314efb36458651
Ahmet Burak Dinç <ahmetburakdincc@gmail.com>
2022-03-26 11:27:49 +0300

    deneme

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+let count = 0
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+function increment(){
+    count++
+    countEl.textContent = count
+}
+function save(){
+    let entries = count + " - "
+    saveEl.textContent += entries
+    count = 0
+    countEl.textContent = count
+}
